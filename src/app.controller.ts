759afba4063928c147add1cbdeb99b2bbaa4b0d1
Avishek <abishek.subedi@spiralogics.com>
2022-10-04 12:17:03 +0545

    local database and get request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Delete, Put } from '@nestjs/common/decorators';
+import { dataBase, ReportType } from './data';
 
-@Controller()
+@Controller('/report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type == 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return dataBase.report.filter((report) => {
+      if (type === 'income') {
+        return report.type == reportType;
+      }
+    });
+    return [];
+  }
+
+  //get by id
+
+  @Get(':id')
+  getReportById() {
     return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'updated';
+  }
+  @Delete(':id')
+  deleteReport() {
+    return 'deleted';
   }
 }
---
+++ src/data.ts
@@ -0,0 +1,61 @@
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const dataBase: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      amount: 65656,
+      created_at: new Date(),
+      updated_at: new Date(),
+      source: 'stealing',
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      amount: 56,
+      created_at: new Date(),
+      updated_at: new Date(),
+      source: 'bank stealing',
+      type: ReportType.EXPENSE,
+    },
+    {
+      id: 'uuid3',
+      amount: 6,
+      created_at: new Date(),
+      updated_at: new Date(),
+      source: 'stealing',
+      type: ReportType.EXPENSE,
+    },
+    {
+      id: 'uuid4',
+      amount: 99,
+      created_at: new Date(),
+      updated_at: new Date(),
+      source: 'Coding',
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
+
+dataBase.report.push({
+  id: 'uuid',
+  source: 'income',
+  amount: 7500,
+  created_at: new Date(),
+  updated_at: new Date(),
+  type: ReportType.INCOME,
+});
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
